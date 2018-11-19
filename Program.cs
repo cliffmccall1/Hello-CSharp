79c67255b5d05d8fe1c86f34c657027087d3973b
Cliff McCall <cliff.mccall1@gmail.com>
2018-11-19 14:09:21 -0600

    first c# code

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace newApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting;
+            greeting = "Hello C#!";
+
+            Console.WriteLine(greeting);
+
+            int age;
+            age = 27;
+
+            Console.WriteLine(age);
+
+            int sum;
+            sum = 2 + 2;
+
+            int newSum;
+            newSum = sum + 10;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(newSum);
+
+            bool isOpen = true;
+            Console.WriteLine(isOpen);
+
+            if (isOpen == false)
+            {
+                Console.WriteLine("Yay!");
+            }
+            else
+            {
+                Console.WriteLine("Sad");
+            }
+        }
+    }
+}
